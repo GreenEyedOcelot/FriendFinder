cc13f88ec958b4f798509cc1b9796699bb738295
GreenEyedOcelot <hbe123@gmail.com>
2018-09-18 17:48:00 -0400

    latest checkin

--- server.js
+++ server.js
@@ -1,27 +1,5 @@
-/*
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var PORT = process.env.PORT || 8080;
-var app = express();
 
-
-app.use(bodyParser.urlencoded( {extended: true}));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-
-require(path.join(__dirname, './app/routing/api-routes.js'))(app);
-require(path.join(__dirname, './app/routing/html-routes.js'))(app);
-app.use(express.static(path.join(__dirname,'./app/public')));
-
-
-
-app.listen(PORT, function() {
-   console.log("FriendFinder is listening on port ", PORT);
-});
-
-*/
-//Dependencies:
+// required
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -29,17 +7,16 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 8250;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
+// body parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json());
 
-//ROUTER
+// routes
 require('./app/routing/api-routes.js')(app); 
 require('./app/routing/html-routes.js')(app);
 
-// Starts the server to begin listening
+// start server
 app.listen(PORT, function () {
-  console.log('App listening on PORT: ' + PORT);
+  console.log('FriendFinder app listening on PORT: ' + PORT);
 });
